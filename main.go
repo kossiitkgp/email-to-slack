60d167960466f5d2b0fcf9d4a5f664d0240a840a
Himanshu Mishra <himanshu2014iit@gmail.com>
2017-12-19 23:11:06 +0530

    Read message.im event data values

--- main.go
+++ main.go
@@ -8,6 +8,18 @@ import (
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
+type E struct {
+    Events string
+}
+
+type MessageIMEventJSON struct {
+    EventType string `json:"type"`
+    Channel string `json:"channel"`
+    User string `json:"user"`
+    Text string `json:"text"`
+    TS string `json:"ts"`
+}
+
 func main() {
 	port := os.Getenv("PORT")
 
@@ -20,12 +32,22 @@ func main() {
 
     router.POST("/", func(c *gin.Context) {
 
-        var json struct {
-            Value string `json:"challenge" binding:"required"`
-        }
+        var json MessageIMEventJSON
+
+        data := &E{}
+
 
         if c.Bind(&json) == nil {
-            c.JSON(200, gin.H{"challenge": json.Value})
+            log.Println("EventType ", json.EventType)
+            log.Println("Channel ", json.Channel)
+            log.Println("User ", json.User)
+            log.Println("Text ", json.Text)
+            log.Println("TS ", json.TS)
+            c.JSON(200, gin.H{"a": "a"})
+        } else if c.Bind(data) == nil {
+            log.Println(data)
+        } else {
+            log.Println("Could not fucking decode")
         }
 
     })
