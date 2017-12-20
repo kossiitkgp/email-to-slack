e6cb344064d320cb79c2168272eb231f11fd98e5
Himanshu Mishra <himanshu2014iit@gmail.com>
2017-12-20 21:20:57 +0530

    Authenticate requests

--- main.go
+++ main.go
@@ -3,27 +3,45 @@ package main
 import (
 	"log"
 	"os"
-    "strings"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
-    "github.com/nlopes/slack"
+	"github.com/nlopes/slack"
 )
 
 type MessageIMEventJSON struct {
-    EventType string `json:"type"`
-    Channel string `json:"channel"`
-    User string `json:"user"`
-    Text string `json:"text"`
-    TS string `json:"ts"`
+	EventType string `json:"type"`
+	Channel   string `json:"channel"`
+	User      string `json:"user"`
+	Text      string `json:"text"`
+	TS        string `json:"ts"`
 }
 
 type SlackPayloadJSON struct {
-    Token string `json:"token"`
-    TeamID string `json:"team_id"`
-    APIAppID string `json:"api_app_id"`
-    EventID string `json:"event_id"`
-    Event MessageIMEventJSON `json:"event"`
+	Token    string             `json:"token"`
+	TeamID   string             `json:"team_id"`
+	APIAppID string             `json:"api_app_id"`
+	EventID  string             `json:"event_id"`
+	Event    MessageIMEventJSON `json:"event"`
+}
+
+func authenticate(payloadjson *SlackPayloadJSON) bool {
+	credentials := [4]string{
+		os.Getenv("SLACK_PAYLOAD_TOKEN"),
+		os.Getenv("TEAM_ID"),
+		os.Getenv("APP_ID"),
+		os.Getenv("MY_DM_CHANNEL"),
+	}
+
+	received_credentials := [4]string{
+		payloadjson.Token,
+		payloadjson.TeamID,
+		payloadjson.APIAppID,
+		payloadjson.Event.Channel,
+	}
+
+	return credentials == received_credentials
 }
 
 func main() {
@@ -36,48 +54,47 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
 
-    router.POST("/", func(c *gin.Context) {
+	router.POST("/", func(c *gin.Context) {
 
-        var payloadjson SlackPayloadJSON
+		var payloadjson SlackPayloadJSON
 
-        if c.Bind(&payloadjson) == nil {
+		bind_err := c.Bind(&payloadjson)
+		valid_req := authenticate(&payloadjson)
 
-            log.Println("Token", payloadjson.Token)
-            log.Println("TeamID", payloadjson.TeamID)
-            log.Println("APIAppID", payloadjson.APIAppID)
-            log.Println("EventID", payloadjson.EventID)
-            log.Println("Event", payloadjson.Event)
+		if bind_err == nil && valid_req {
+			log.Println("Token", payloadjson.Token)
+			log.Println("TeamID", payloadjson.TeamID)
+			log.Println("APIAppID", payloadjson.APIAppID)
+			log.Println("EventID", payloadjson.EventID)
+			log.Println("Event", payloadjson.Event)
 
-            log.Println("EventType ", payloadjson.Event.EventType)
-            log.Println("Channel ", payloadjson.Event.Channel)
-            log.Println("User ", payloadjson.Event.User)
-            log.Println("Text ", payloadjson.Event.Text)
-            log.Println("TS ", payloadjson.Event.TS)
+			log.Println("EventType ", payloadjson.Event.EventType)
+			log.Println("Channel ", payloadjson.Event.Channel)
+			log.Println("User ", payloadjson.Event.User)
+			log.Println("Text ", payloadjson.Event.Text)
+			log.Println("TS ", payloadjson.Event.TS)
 
-            // Prepare the message to be sent
-            slackbot_message := strings.Split(payloadjson.Event.Text, "|")[0]
-            link := strings.Split(slackbot_message, " ")
-            message_to_send := `Received an email ! Somebody reply soon. Content : ` + link[len(link) - 1][1:]
-            log.Println(message_to_send)
+			// Prepare the message to be sent
+			slackbot_message := strings.Split(payloadjson.Event.Text, "|")[0]
+			link := strings.Split(slackbot_message, " ")
+			message_to_send := `Received a new email. Content : ` + link[len(link)-1][1:]
+			log.Println(message_to_send)
 
-            // Send the message
-            api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
-            params := slack.NewPostMessageParameters()
-            params.Text = message_to_send
-            params.Username = "bhattu"
-            params.AsUser = true
-            params.Parse = "full"
+			// Send the message
+			api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+			params := slack.NewPostMessageParameters()
+			params.Text = message_to_send
+			params.Username = "bhattu"
+			params.AsUser = true
+			params.Parse = "full"
 
-            api.PostMessage(os.Getenv("CHANNEL_ID"), message_to_send, params)
+			api.PostMessage(os.Getenv("CHANNEL_ID"), message_to_send, params)
 
-            c.JSON(200, gin.H{"success": true})
-
-        } else {
-            log.Println("Could not fucking decode payloadjson")
-            c.String(404, "Get the Fuck Off")
-        }
-
-    })
+			c.JSON(200, gin.H{"success": true})
+		} else {
+			c.JSON(404, gin.H{"success": false})
+		}
+	})
 
 	router.Run(":" + port)
 }
