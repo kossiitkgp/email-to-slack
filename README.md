e8e34bbc6aecda71f7912f5782829fa4fc3a4681
Himanshu Mishra <himanshu@orkohunter.net>
2019-01-25 17:55:25 +0530

    WIP

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 If your team works on a Slack workspace and the team has to maintain a bunch of different email ids and wish to
 be notified of every email instantly on slack; there is a hack for you.
+
+
+- Create an app
+- Add scopes 2
+- Activate incoming webhook
+- Install it in your workspace
+- Install in one channel. You can install on multiple channels later
+-
--- app.py
+++ app.py
@@ -11,16 +11,62 @@ def create_app():
     @app.route("/", methods=['GET', 'POST'])
     def main():
         if request.method == "GET":
-            return "Hello World"
+            return redirect("https://github.com/kossiitkgp/email-to-slack")
         elif request.method == "POST":
-            print("parameters")
-            print(json.dumps(request.get_json(force=True)))
+            # print("parameters")
+            # print(json.dumps(request.get_json(force=True)))
+            # print("headers")
+            # print(request.headers)
+            params = json.dumps(request.get_json(force=True))
+            email = params["event"]["files"][0]
 
-            print("headers")
-            print(request.headers)
+            INCOMING_WEBHOOK_URL = os.environ["INCOMING_WEBHOOK_URL"]
 
-            # params = request.get_json(force=True)
-            # print(params)
+            headers = {
+                "Content-type": "application/json"
+            }
+
+
+            sender_email = email["from"][0]["original"]
+            email_file_link = email["url_private"]
+            email_subject = email["title"]
+            email_content = "```" + email["plain_text"] + "```"
+            timestamp = email["timestamp"]
+            koss_logo = "https://raw.githubusercontent.com/kossiitkgp/design/master/logo/exported/koss-logo.png"
+            koss_logo_small = "https://raw.githubusercontent.com/kossiitkgp/design/master/logo/exported/koss-filled-small.png"
+
+            data = {
+                "text": f"\n*New mail in the inbox*! Click here : \n<{email_file_link}|Email>\n",
+                "attachments": [
+                    {
+                        "fallback": email_file_link,
+                        "color": "#36a64f",
+                        "pretext": "Click on the file for better view. Although here are some details.",
+                        "author_name": sender_email,
+                        "author_link": email_file_link,
+                        "author_icon": koss_logo_small,
+                        "title": email_subject,
+                        "title_link": email_file_link,
+                        "text": email_content,
+                        "fields": [
+                            {
+                                "title": "This message also has attachements",
+                                "value": f"<{email_file_link}|View complete email>",
+                                "short": True
+                            }
+                        ],
+                        "footer": "email-to-slack",
+                        "footer_icon": koss_logo_small,
+                        "ts": timestamp
+                    }
+                ]
+            }
+            r = requests.post(INCOMING_WEBHOOK_URL, json=data)
+
+            """
+            Enable this to verify the URL while installing the app
+            """
+
             # data = {
             #     'challenge': params.get('challenge'),
             # }
@@ -32,7 +78,6 @@ def create_app():
             # resp.headers['Content-type'] = 'application/json'
 
             # return resp
-            return ""
 
     app.secret_key = os.environ.setdefault("APP_SECRET_KEY", "notsosecret")
     app.config['SESSION_TYPE'] = 'filesystem'
