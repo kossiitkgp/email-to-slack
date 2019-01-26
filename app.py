77382ad0309cacce5ec62419f975d4790d098c80
xypnox <xypnox@gmail.com>
2019-01-26 20:11:13 +0530

    verify heroku

--- app.py
+++ app.py
@@ -30,6 +30,17 @@ def main():
     if request.method == "GET":
         return redirect("https://github.com/kossiitkgp/email-to-slack")
     elif request.method == "POST":
+        data = {
+            'challenge': params.get('challenge'),
+        }
+        resp = Response(
+            response=json.dumps(data),
+            status=200,
+            mimetype='application/json'
+        )
+        resp.headers['Content-type'] = 'application/json'
+
+        return resp
         print("parameters")
         print(json.dumps(request.get_json(force=True)))
         print("\n\n\n\nheaders\n\n\n\n")
