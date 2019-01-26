d28e327a42d35214f37af8210cdcc06ed6f4702b
xypnox <xypnox@gmail.com>
2019-01-26 22:12:34 +0530

    modified variables to test the emails

--- app.py
+++ app.py
@@ -79,13 +79,13 @@ def main():
                         "pretext": "",
                         "author_name": sender_email,
                         "author_link": email_provider,
-                        # "author_icon": koss_logo_small,
+                        "author_icon": "",
                         "title": email_subject,
                         "title_link": email_provider,
                         "text": email_content,
                         "fields": [],
-                        # "footer": "email-to-slack",
-                        # "footer_icon": koss_logo_small,
+                        "footer": "",
+                        "footer_icon": "",
                         "ts": "⏰" + timestamp
                     }
                 ]
